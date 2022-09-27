64303ae9c6ad0a6975d494af644b0ee522535c32
arnaldocarpi <arnaldo.carpi@icloud.com>
2022-09-26 21:45:40 -0300

    Creating and Stylizing Texts from the HomePage

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
 import Frontend from '../templates/Frontend'
+import * as S from './styles'
+
 
 function Main() {
-  return <Frontend>BUCETA</Frontend>
+  return (
+  <Frontend>
+    <S.Container>
+      <div>
+        <h1>Design driven development of your web product</h1>
+        <h6>We are a full service digital agency that builds immesive user experience.</h6>
+      </div>
+      <div>Coluna 2</div>
+    </S.Container>
+
+  </Frontend>
+  )
 }
 
 export default Main
--- src/templates/Frontend/index.tsx
+++ src/templates/Frontend/index.tsx
@@ -8,6 +8,7 @@ const Frontend = ({children}: FrontendProps) => {
     return (
         <S.Container>
             <Header></Header>
+            <main>{ children }</main>
         </S.Container>
     )
 }
